1eef2f62eefc20eccbec2358bc43040c765710e1
damienjacinto <damien.jacinto@gmail.com>
2021-11-16 22:36:03 +0100

    fix sleep

--- terraform/modules/certmanager/main.tf
+++ terraform/modules/certmanager/main.tf
@@ -23,7 +23,7 @@ resource "time_sleep" "wait_60_seconds" {
 }
 
 resource "kubectl_manifest" "certmanager_issuer" {
-  depends_on = [time_sleep.wait_30_seconds]
+  depends_on = [time_sleep.wait_60_seconds]
   yaml_body  = templatefile("${path.module}/templates/clusterissuer.yml", {
     namespace        = kubernetes_namespace.certmanager.metadata[0].name
     name             = var.certmanager_clusterissuer_name
